f5365b5d0a9d34b2c2174d1e4d3f50411454ebce
gulartt <samuelgulart@outlook.com>
2022-03-08 10:26:10 -0300

    Atualizações visuais

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Apenas um projeto em andamento feito em C que simula um sistema bancário.
 
 # Histórico de atualizações
 
-Ultima atualização: 24/02 - Iniciei a criação de um sistema de extrato 
+24/02 - Iniciei a criação de um sistema de extrato 
 
+08/03 - Melhorias visuais
--- sistemadebanco.c
+++ sistemadebanco.c
@@ -5,7 +5,7 @@
 #include <conio.h>
 
 // Samuel Gulart Moura - https://www.linkedin.com/in/samuel-gulart-656971216 - 27/12/2021
-// Ultima alteração: 24/02
+// Ultima alteração: 08/03
 
 typedef struct{
     char nome[30], cpf[20], senha[20];
@@ -28,7 +28,8 @@ void quantidadeDeCedulas(int valor, int *c1, int *c5, int *c10, int *c50, int *c
 int extrato(char * nome, char * senha, int qtdContas);
 
 void main(void){
-    int novaConta = 0, checkaConta, valor, c1, c5, c10, c50, c100, c200;
+    int checkaConta, valor, c1, c5, c10, c50, c100, c200;
+    unsigned int novaConta = 0;
     char escolha, nome[20], senha[20];
     float checkaExtrato;
     setlocale(LC_ALL, "Portuguese");
@@ -40,12 +41,15 @@ void main(void){
 
         if(escolha == '1'){
             limpaTela();
-            printf("================== CADASTRO DE CONTAS ===================\n\n");
-            printf("Digite seu nome: ");
+            printf("  ___   _   ___   _   ___ _____ ___  ___   ");
+            printf("\n / __| /_\\ |   \\ /_\\ / __|_   _| _ \\/ _ \\  ");
+            printf("\n| (__ / _ \\| |) / _ \\\\__ \\ | | |   / (_) | ");
+            printf("\n \\___/_/ \\_\\___/_/ \\_\\___/ |_| |_|_\\\\___/  \n\n");
+            printf("Digite seu nome  ");
             scanf("%s", &conta[novaConta].nome);
-            printf("Digite sua idade: ");
+            printf("Digite sua idade  ");
             scanf("%i", &conta[novaConta].idade);
-            printf("Digite seu CPF: ");
+            printf("Digite seu CPF  ");
             scanf("%s", &conta[novaConta].cpf);
             printf("Digite uma senha (Apenas Números): ");
             scanf("%s", &conta[novaConta].senha);
@@ -58,23 +62,31 @@ void main(void){
             limpaTela();
         }else if(escolha == '2'){
             limpaTela();
-            printf("========================= SAQUE =========================\n\n");
+            printf("  ___   _   ___  _   _ ___ ");
+            printf("\n / __| /_\\ / _ \\| | | | __|");
+            printf("\n \\__ \\/ _ \\ (_) | |_| | _| ");
+            printf("\n |___/_/ \\_\\__\\_\\\\___/|___|\n\n");
             printf("Informe o valor do Saque [NOTAS DISPONIVEIS: 1$ 5$ 10$ 50$ 100$ 200$]: ");
             scanf("%d", &valor);
 
             quantidadeDeCedulas(valor, &c1, &c5, &c10, &c50, &c100, &c200);
+            printf("\n");
             if (c200 != 0) printf("%i cédulas de 200\n", c200);
             if (c100 != 0) printf("%i cédulas de 100\n", c100);
             if (c50 != 0) printf("%i cédulas de 50\n", c50);
             if (c10 != 0) printf("%i cédulas de 10\n", c10);
             if (c5 != 0) printf("%i cédulas de 5\n", c5);
             if (c1 != 0) printf("%i cédulas de 1\n", c1);
-            printf("\n=========================================================\n\n");
+            printf("\n");
             system("PAUSE");
             limpaTela();
         }else if(escolha == '3'){
             limpaTela();
-            printf("================== BANCO DEV ===================\n\n");
+            printf(" ____    _    _   _  ____ ___    ____  _______     __");
+            printf("\n| __ )  / \\  | \\ | |/ ___/ _ \\  |  _ \\| ____\\ \\   / /");
+            printf("\n|  _ \\ / _ \\ |  \\| | |  | | | | | | | |  _|  \\ \\ / / ");
+            printf("\n| |_) / ___ \\| |\\  | |__| |_| | | |_| | |___  \\ V / ");
+            printf("\n|____/_/   \\_\\_| \\_|\\____\\___/  |____/|_____|  \\_/  \n\n");
             for(int x = 0; x < novaConta; x++){
                 printf("Cliente[%i]: %s - CPF: %s\n", x+1, conta[x].nome, conta[x].cpf);
             }
@@ -83,7 +95,11 @@ void main(void){
             limpaTela();
         }else if(escolha == '4'){
             limpaTela();
-            printf("================== BANCO DEV ===================\n\n");
+            printf(" ____    _    _   _  ____ ___    ____  _______     __");
+            printf("\n| __ )  / \\  | \\ | |/ ___/ _ \\  |  _ \\| ____\\ \\   / /");
+            printf("\n|  _ \\ / _ \\ |  \\| | |  | | | | | | | |  _|  \\ \\ / / ");
+            printf("\n| |_) / ___ \\| |\\  | |__| |_| | | |_| | |___  \\ V / ");
+            printf("\n|____/_/   \\_\\_| \\_|\\____\\___/  |____/|_____|  \\_/  \n\n");
             printf("Insira o Login: ");
             scanf("%s", &nome);
             printf("Insira a senha: ");
@@ -111,14 +127,18 @@ void limpaTela(void)
 }
 
 void imprimeMenu(void){
-    printf("================== BANCO DEV ===================\n\n");
+    printf(" ____    _    _   _  ____ ___    ____  _______     __");
+    printf("\n| __ )  / \\  | \\ | |/ ___/ _ \\  |  _ \\| ____\\ \\   / /");
+    printf("\n|  _ \\ / _ \\ |  \\| | |  | | | | | | | |  _|  \\ \\ / / ");
+    printf("\n| |_) / ___ \\| |\\  | |__| |_| | | |_| | |___  \\ V / ");
+    printf("\n|____/_/   \\_\\_| \\_|\\____\\___/  |____/|_____|  \\_/  \n\n");
     printf("Seja bem vindo ao Banco DEV!\n");
-    printf("escolha uma opção abaixo: ");
-    printf("\n1 - Criar conta");
-    printf("\n2 - Saque");
-    printf("\n3 - Listar contas ativas");
-    printf("\n4 - Extrato");
-    printf("\n5 - Sair\n\n");
+    printf("escolha uma opção abaixo: \n");
+    printf("\n1   Criar conta");
+    printf("\n2   Saque");
+    printf("\n3   Listar contas ativas");
+    printf("\n4   Extrato");
+    printf("\n5   Sair\n\n");
 }
 
 int verificaIdade(int x){
@@ -130,17 +150,22 @@ int validaConta(char * nome,  int idade, char * cpf){
     x = verificaIdade(idade);
     if(x == 1){
         limpaTela();
-        printf("================== SUA CONTA FOI CRIADA COM SUCESSO ===================\n\n");
-        printf("=========================== Dados da conta! ===========================\n\n");
-        printf("Nome do cliente: %s\n", nome);
-        printf("Idade do cliente: %i\n", idade);
-        printf("CPF do cliente: %s\n\n", cpf);
-        printf("=======================================================================\n\n");
+        printf("  ___ _   _  ___ ___ ___ ___  ___  _ ");
+        printf("\n / __| | | |/ __| __/ __/ __|/ _ \\| |");
+        printf("\n \\__ \\ |_| | (__| _|\\__ \\__ \\ (_) |_|");
+        printf("\n |___/\\___/ \\___|___|___/___/\\___/(_)\n\n");
+        printf("Dados da conta: \n\n");
+        printf("Nome do cliente  %s\n", nome);
+        printf("Idade do cliente  %i\n", idade);
+        printf("CPF do cliente  %s\n\n", cpf);
         return 1;
     }else{
-        printf("================== CADASTRO DE CONTAS ===================\n\n");
-        printf("Não foi possivel criar a conta pois você é menor de idade!!\n\n");
-        printf("=========================================================\n\n");
+        limpaTela();
+        printf("  ___ ___ ___  ___  ");
+        printf("\n | __| _ \\ _ \\/ _ \\ ");
+        printf("\n | _||   /   / (_) |");
+        printf("\n |___|_|_\\_|_\\\\___/ \n\n");
+        printf("Não permitimos o cadastro de menores de idade!\n\n");
         return 0;
     }
 }
