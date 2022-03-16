68d47aa77b5b64aa5158e2bdaf42126b2fd4d2a2
gulartt <samuelgulart@outlook.com>
2022-03-15 22:52:19 -0300

    atualizações no saque

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Apenas um projeto em andamento feito em C que simula um sistema bancário.
 24/02 - Iniciei a criação de um sistema de extrato 
 
 08/03 - Melhorias visuais
+
+15/03 - Mudanças no sistema de saque
--- sistemadebanco.c
+++ sistemadebanco.c
@@ -5,7 +5,7 @@
 #include <conio.h>
 
 // Samuel Gulart Moura - https://www.linkedin.com/in/samuel-gulart-656971216 - 27/12/2021
-// Ultima alteração: 08/03
+// Ultima alteração: 15/03
 
 typedef struct{
     char nome[30], cpf[20], senha[20];
@@ -27,8 +27,10 @@ void quantidadeDeCedulas(int valor, int *c1, int *c5, int *c10, int *c50, int *c
 
 int extrato(char * nome, char * senha, int qtdContas);
 
+int loga(char * nome, char * senha, int qtdContas);
+
 void main(void){
-    int checkaConta, valor, c1, c5, c10, c50, c100, c200;
+    int checkaConta, valor, c1, c5, c10, c50, c100, c200, logado;
     unsigned int novaConta = 0;
     char escolha, nome[20], senha[20];
     float checkaExtrato;
@@ -47,12 +49,16 @@ void main(void){
             printf("\n \\___/_/ \\_\\___/_/ \\_\\___/ |_| |_|_\\\\___/  \n\n");
             printf("Digite seu nome  ");
             scanf("%s", &conta[novaConta].nome);
+            setbuf(stdin, NULL);
             printf("Digite sua idade  ");
             scanf("%i", &conta[novaConta].idade);
+            setbuf(stdin, NULL);
             printf("Digite seu CPF  ");
             scanf("%s", &conta[novaConta].cpf);
+            setbuf(stdin, NULL);
             printf("Digite uma senha (Apenas Números): ");
             scanf("%s", &conta[novaConta].senha);
+            setbuf(stdin, NULL);
             checkaConta = validaConta(conta[novaConta].nome, conta[novaConta].idade, conta[novaConta].cpf);
             if(checkaConta == 1){
                 conta[novaConta].saldo = 3001 + ( rand() % 7000 );
@@ -66,19 +72,42 @@ void main(void){
             printf("\n / __| /_\\ / _ \\| | | | __|");
             printf("\n \\__ \\/ _ \\ (_) | |_| | _| ");
             printf("\n |___/_/ \\_\\__\\_\\\\___/|___|\n\n");
-            printf("Informe o valor do Saque [NOTAS DISPONIVEIS: 1$ 5$ 10$ 50$ 100$ 200$]: ");
-            scanf("%d", &valor);
-
-            quantidadeDeCedulas(valor, &c1, &c5, &c10, &c50, &c100, &c200);
-            printf("\n");
-            if (c200 != 0) printf("%i cédulas de 200\n", c200);
-            if (c100 != 0) printf("%i cédulas de 100\n", c100);
-            if (c50 != 0) printf("%i cédulas de 50\n", c50);
-            if (c10 != 0) printf("%i cédulas de 10\n", c10);
-            if (c5 != 0) printf("%i cédulas de 5\n", c5);
-            if (c1 != 0) printf("%i cédulas de 1\n", c1);
-            printf("\n");
-            system("PAUSE");
+            printf("Insira seu login [nome da conta]: ");
+            scanf("%s", &nome);
+            setbuf(stdin, NULL);
+            printf("Agora insira sua senha: ");
+            scanf("%s", &senha);
+            setbuf(stdin, NULL);
+            logado = loga(nome, senha, novaConta);
+            if(logado != -1){
+                limpaTela();
+                printf("  ___   _   ___  _   _ ___ ");
+                printf("\n / __| /_\\ / _ \\| | | | __|");
+                printf("\n \\__ \\/ _ \\ (_) | |_| | _| ");
+                printf("\n |___/_/ \\_\\__\\_\\\\___/|___|\n\n");
+                printf("seu saldo é de: %.2f\n", conta[logado].saldo);
+                printf("Informe o valor do Saque [NOTAS DISPONIVEIS: 1$ 5$ 10$ 50$ 100$ 200$]: ");
+                scanf("%d", &valor);
+                setbuf(stdin, NULL);
+                if(valor > conta[logado].saldo){
+                    system("COLOR 0C");
+                    printf("\nVALOR INSUFICIENTE, CANCELANDO A OPERAÇÃO\n\n");
+                    Sleep(3000);
+                    system("COLOR 07");
+                }else{
+                    quantidadeDeCedulas(valor, &c1, &c5, &c10, &c50, &c100, &c200);
+                    printf("\n");
+                    if (c200 != 0) printf("%i cédulas de 200\n", c200);
+                    if (c100 != 0) printf("%i cédulas de 100\n", c100);
+                    if (c50 != 0) printf("%i cédulas de 50\n", c50);
+                    if (c10 != 0) printf("%i cédulas de 10\n", c10);
+                    if (c5 != 0) printf("%i cédulas de 5\n", c5);
+                    if (c1 != 0) printf("%i cédulas de 1\n", c1);
+                    conta[logado].saldo = conta[logado].saldo - valor;
+                    printf("\n");
+                    system("PAUSE");
+                }
+            }
             limpaTela();
         }else if(escolha == '3'){
             limpaTela();
@@ -100,16 +129,18 @@ void main(void){
             printf("\n|  _ \\ / _ \\ |  \\| | |  | | | | | | | |  _|  \\ \\ / / ");
             printf("\n| |_) / ___ \\| |\\  | |__| |_| | | |_| | |___  \\ V / ");
             printf("\n|____/_/   \\_\\_| \\_|\\____\\___/  |____/|_____|  \\_/  \n\n");
-            printf("Insira o Login: ");
+            printf("Insira o Login [nome da conta]: ");
             scanf("%s", &nome);
+            setbuf(stdin, NULL);
             printf("Insira a senha: ");
             scanf("%s", &senha);
-            checkaExtrato = extrato(nome, senha, novaConta);
-            if(checkaExtrato == 0){
+            setbuf(stdin, NULL);
+            logado = loga(nome, senha, novaConta);
+            if(logado == -1){
                 system("COLOR 0C");
                 printf("\nSENHA INCORRETA OU CONTA INEXISTENTE, CANCELANDO A OPERAÇÃO!!\n\n");
             }else{
-                printf("\nO extrato da sua conta bancaria é de: %.2f R$\n\n", checkaExtrato);
+                printf("\nO extrato da sua conta bancaria é de: %.2f R$\n\n", conta[logado].saldo);
             }
             Sleep(3000);
             system("COLOR 07");
@@ -121,10 +152,14 @@ void main(void){
     exit(0);
 }
 
+// Limpa a tela
+
 void limpaTela(void)
 {
     system("@cls||clear");
 }
+
+// Imprime o Menu
 
 void imprimeMenu(void){
     printf(" ____    _    _   _  ____ ___    ____  _______     __");
@@ -141,9 +176,13 @@ void imprimeMenu(void){
     printf("\n5   Sair\n\n");
 }
 
+// Verifica se o cliente é maior de idade
+
 int verificaIdade(int x){
     if (x >= 18) return 1;
 }
+
+// Valida e Cria a Conta
 
 int validaConta(char * nome,  int idade, char * cpf){
     int x;
@@ -170,6 +209,8 @@ int validaConta(char * nome,  int idade, char * cpf){
     }
 }
 
+// Faz a contagem de cédulas pro saque
+
 void quantidadeDeCedulas(int valor, int *c1, int *c5, int *c10, int *c50, int *c100, int * c200){
     *c200 = valor /200;
     valor = valor % 200;
@@ -184,16 +225,17 @@ void quantidadeDeCedulas(int valor, int *c1, int *c5, int *c10, int *c50, int *c
     *c1 = valor;
 }
 
-int extrato(char * nome, char * senha, int qtdContas){
-    int retorno;
+// Confere se o login e a senha enviados batem com alguma dascontas registradas
 
-    for(int i = 0; i < qtdContas; i++){
-        retorno = strcmp(conta[i].nome, nome);
-        if(retorno == 0){
-            retorno = strcmp(conta[i].senha, senha);
-            if(retorno == 0) return conta[i].saldo;
+int loga(char * nome, char * senha, int qtdContas){
+    int valor;
+    for(int i = 0; i < qtdContas; ++i){
+        valor = strcmp(conta[i].nome, nome);
+        if(valor == 0){
+            valor = strcmp(conta[i].senha, senha);
+            if(valor == 0) return i;
         }
     }
 
-    return 0;
+    return -1;
 }
